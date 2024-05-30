90b203555a738ce7397ec8e6e1304b5b5a839357
pkoranteng <p_addo@hotmail.com>
2024-05-31 00:17:34 +0100

    Import project

---
+++ client/src/common/__init__.py
@@ -0,0 +1,31 @@
+# __pragma__ ('skip')
+"""
+These JavaScript builtin funcion and object stubs are just to quiet the Python linkter and are ignored by transcrypt as long
+as they are imported inside of pragma skip/noskip lines.
+"""
+def require(lib):
+    return lib
+def __new__(obj):
+    return obj
+class JSON:
+    stringify = None
+class document:
+    title = None
+    getElementById = None
+    addEventListener = None
+class window:
+    class console:
+        log = None
+        error = None
+        warn = None
+    alert = None
+    confirm = None
+    fetch = None
+    history = None
+    location = None
+    addEventListener = None
+    dispatchEvent = None
+    PopStateEvent = None
+    URLSearchParams = None
+    encodeURIComponent = None
+# __pragma__ ('noskip')
